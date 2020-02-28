d634c7fc73a1100dbb620ea641d1e2a63b395fd0
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2020-02-28 17:37:13 -0500

    Adding Exercise 2.2

---
+++ week-2/kobyluck-exercise-2.2.js
@@ -0,0 +1,53 @@
+/*
+============================================
+; Title:  Exercise 2.2
+; Author: Professor Krasso
+; Date:   26 February 2020
+; Modified By: Jonathan Kobyluck
+; Description: Prototypes
+;===========================================
+*/
+
+// Require statement that imports my header file
+const header = require('../kobyluck-header.js');
+
+// Calling the console log function to display my header
+console.log(header.display('Johnny', 'Kobyluck', 'Exercise 2.2'));
+
+// Line break
+console.log(" ")
+
+/*
+ Expected output:
+
+ FirstName LastName
+ <AssignmentName>
+ <Today's Date>
+
+The person's full name is: 'Jim Duncan'
+The person's age is: '25'
+*/
+
+// declaring object literal "Person"
+var person = {
+    // returning the person's age
+    getAge: function() {
+        return this.age;
+    }
+};
+
+// 
+var jim = Object.create(person, {
+    "age": {
+        "value": "25"
+    },
+    "fullname": {
+        "value": "Jim Duncan"
+    }
+});
+
+jim.getAge(); 
+
+// console log output
+console.log("The person's full name is: '%s'", jim.fullname);
+console.log("The person's age is: '%s'", jim.age);
